46365d98f671e0962d11374e83e7d553020cd5c8
0xK3K <0xk3k@protonmail.com>
2023-12-11 16:11:25 +0700

    Husky

--- src/components/StarknetConfigWithConnectors/index.tsx
+++ src/components/StarknetConfigWithConnectors/index.tsx
@@ -1,15 +1,9 @@
-import { ReactNode, useMemo } from "react";
-import { goerli, mainnet } from "@starknet-react/chains";
-import {
-  argent,
-  braavos,
-  Connector,
-  jsonRpcProvider,
-  StarknetConfig,
-} from "@starknet-react/core";
-import { constants } from "starknet";
-import { ArgentMobileConnector } from "starknetkit/dist/connectors";
-import { WebWalletConnector } from "starknetkit/dist/connectors";
+import { ReactNode, useMemo } from 'react'
+import { goerli, mainnet } from '@starknet-react/chains'
+import { argent, braavos, Connector, jsonRpcProvider, StarknetConfig } from '@starknet-react/core'
+import { constants } from 'starknet'
+import { ArgentMobileConnector } from 'starknetkit/dist/connectors'
+import { WebWalletConnector } from 'starknetkit/dist/connectors'
 
 export default function StarknetConfigWithConnectors({ children }: { children: ReactNode }) {
   const { chains, provider, connectors } = useMemo(
@@ -17,43 +11,38 @@ export default function StarknetConfigWithConnectors({ children }: { children: R
       connectors: [
         argent(),
         new ArgentMobileConnector({
-          dappName: "Stargaze App",
-          description: "Web application for interacting with Stargaze protocol",
+          dappName: 'Stargaze App',
+          description: 'Web application for interacting with Stargaze protocol',
           url: window.location.origin,
-          chainId: constants.NetworkName.SN_MAIN,
+          chainId: constants.NetworkName.SN_MAIN
         }),
         braavos(),
-        new WebWalletConnector({ url: "https://web.argent.xyz" }),
+        new WebWalletConnector({ url: 'https://web.argent.xyz' })
       ] as Connector[],
       provider: jsonRpcProvider({
         rpc: (chain) => {
           if (chain.id === mainnet.id) {
             return {
               chainId: constants.StarknetChainId.SN_MAIN,
-              nodeUrl: `https://starknet-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_STARKNET_ALCHEMY_KEY}`,
-            };
+              nodeUrl: `https://starknet-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_STARKNET_ALCHEMY_KEY}`
+            }
           } else if (chain.id === goerli.id) {
             return {
               chainId: constants.StarknetChainId.SN_GOERLI,
-              nodeUrl: `https://starknet-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_STARKNET_GOERLI_ALCHEMY_KEY}`,
-            };
+              nodeUrl: `https://starknet-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_STARKNET_GOERLI_ALCHEMY_KEY}`
+            }
           }
-          throw new Error(`Unrecognized chain ID: ${chain.id}`);
-        },
+          throw new Error(`Unrecognized chain ID: ${chain.id}`)
+        }
       }),
-      chains: [mainnet, goerli],
+      chains: [mainnet, goerli]
     }),
     []
-  );
+  )
 
   return (
-    <StarknetConfig
-      chains={chains}
-      provider={provider}
-      connectors={connectors}
-      autoConnect
-    >
+    <StarknetConfig chains={chains} provider={provider} connectors={connectors} autoConnect>
       {children}
     </StarknetConfig>
-  );
+  )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { NextUIProvider } from '@nextui-org/react'
 import Layout from '@/components/Layout'
 
 import '@/styles/globals.css'
-import StarknetConfigWithConnectors from "@/components/StarknetConfigWithConnectors";
+import StarknetConfigWithConnectors from '@/components/StarknetConfigWithConnectors'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,9 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <StarknetConfigWithConnectors>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </StarknetConfigWithConnectors>
     </NextUIProvider>
   )
