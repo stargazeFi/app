ec54d59c29807feba24a60c3c937e07e25bef5e4
0xK3K <0xk3k@protonmail.com>
2023-12-13 11:56:33 +0700

    Placeholder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import StarknetConfigWrapper from '@/StarknetConfigWrapper'
+// import StarknetConfigWrapper from '@/StarknetConfigWrapper'
 import App from '@/App'
 
 const queryClient = new QueryClient()
@@ -10,11 +10,11 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <StarknetConfigWrapper>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </StarknetConfigWrapper>
+      {/* <StarknetConfigWrapper> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </StarknetConfigWrapper> */}
     </QueryClientProvider>
   </StrictMode>
 )
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -3,17 +3,21 @@ import { MainText } from '@/components/Text'
 import { Image } from '@nextui-org/react'
 
 export default function Home() {
+  const rng = Math.floor(Math.random() * 10) + 1
+  const image = rng === 10 ? '/assets/general/error-pepe.png' : `/assets/general/error-${(rng % 3) + 1}.png`
+
   return (
     <Container>
       <Box>
-        <Image
-          src={`/assets/general/error-${Math.floor(Math.random() * 3) + 1}.png`}
-          className='mt-10 h-[40vw] w-auto md:h-[30vw]'
-        />
-        <MainText heading size='2xl'>
+        <Image src={image} className='mt-10 h-[40vw] w-auto md:h-[30vw]' />
+        {/* <MainText heading size='2xl'>
           Page not found
         </MainText>
-        <MainText>The page you requested does not exist</MainText>
+        <MainText>The page you requested does not exist</MainText> */}
+        <MainText heading size='2xl'>
+          Too early!
+        </MainText>
+        <MainText>You're an early Pepe. We need more time to build, but thanks for checking us out :3</MainText>
       </Box>
     </Container>
   )
