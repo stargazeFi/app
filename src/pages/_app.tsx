986983078e6ffc46f25d4c949e622014183ca6df
0xK3K <0xk3k@protonmail.com>
2024-01-10 13:24:45 +0700

    Update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='icon' href='/favicon/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='192x192' href='/favicon/android-chrome-192x192.png' />
+        <link rel='icon' type='image/png' sizes='512x512' href='/favicon/android-chrome-512x512.png' />
+        <link rel='apple-touch-icon' type='image/png' href='/favicon/apple-touch-icon.png' />
       </Head>
       <NextUIProvider>
         <main className='text-foreground dark'>
