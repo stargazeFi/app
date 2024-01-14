948e97149884ee89024cb6588b1e2af9c0fb5f50
0xK3K <0xk3k@protonmail.com>
2024-01-14 07:18:28 +0700

    Add goerli header and placekitten

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,55 +1,65 @@
 import { useAppSelector } from '@/hooks'
 import { selectPendingTransactions } from '@/store/appSlice'
+import { useNetwork } from '@starknet-react/core'
 import Link from 'next/link'
 import { Image, Navbar, NavbarBrand, NavbarContent, NavbarItem, Spinner } from '@nextui-org/react'
-import { CurrencyExchange } from '@mui/icons-material'
+import { AnalyticsOutlined, MonetizationOnOutlined } from '@mui/icons-material'
 import { Box, MainText } from '@/components/Layout'
 import WalletModal from '@/components/WalletModal'
 import { useRouter } from 'next/router'
+import { goerli } from '@starknet-react/chains'
 
 export default function Header() {
-  const pendingTransactions = useAppSelector(selectPendingTransactions)
+  const { chain } = useNetwork()
   const { pathname } = useRouter()
+  const pendingTransactions = useAppSelector(selectPendingTransactions)
 
   const routes = [
-    { url: '/', name: 'Strategies', icon: <CurrencyExchange fontSize='inherit' className='text-gray-200' /> }
+    { url: '/', name: 'Strategies', icon: <MonetizationOnOutlined className='text-gray-200' /> },
+    { url: '/analytics', name: 'Analytics', icon: <AnalyticsOutlined className='text-gray-200' /> }
     // { url: '/dashboard', name: 'Dashboard' }
-    // { url: '/analytics', name: 'Analytics' }
   ]
 
   return (
-    <Navbar position='static' isBlurred={false} maxWidth='xl' className='mb-10 bg-transparent'>
-      <NavbarBrand>
-        <Link href='https://stargaze.finance'>
-          <Image src='/assets/brand/logo.svg' width={40} height={40} alt='' />
-        </Link>
-      </NavbarBrand>
-      <NavbarContent justify='center' className='flex gap-10 xl:gap-20'>
-        {routes.map(({ url, name, icon }, index) => (
-          <NavbarItem key={index} className={pathname !== url ? 'cursor-pointer' : ''} isActive={pathname === url}>
-            <Link href={pathname === url ? '' : url}>
-              <Box center>
-                {icon}
-                <MainText gradient className='ml-2'>
-                  {name}
-                </MainText>
-              </Box>
-            </Link>
-          </NavbarItem>
-        ))}
-      </NavbarContent>
-      <NavbarContent justify='end'>
-        {!!pendingTransactions.length && (
-          <Box center className='relative rounded-3xl border border-gray-500 bg-black/60 p-0.5'>
-            <MainText className='absolute'>{pendingTransactions.length}</MainText>
-            <Spinner />
-          </Box>
-        )}
-        <WalletModal />
-        <Box center className='rounded-md bg-red-900 px-3 py-1.5'>
-          <MainText className='text-xs'>Alpha v0.3.0</MainText>
+    <>
+      {chain.id !== goerli.id && (
+        <Box center className='h-8 w-full bg-red-900'>
+          <MainText>Stargaze is currently in alpha, please switch network to Goerli</MainText>
         </Box>
-      </NavbarContent>
-    </Navbar>
+      )}
+      <Navbar position='static' isBlurred={false} maxWidth='xl' className='mb-10 bg-transparent'>
+        <NavbarBrand>
+          <Link href='https://stargaze.finance'>
+            <Image src='/assets/brand/logo.svg' width={40} height={40} alt='' />
+          </Link>
+        </NavbarBrand>
+        <NavbarContent justify='center' className='flex gap-10 xl:gap-20'>
+          {routes.map(({ url, name, icon }, index) => (
+            <NavbarItem key={index} className={pathname !== url ? 'cursor-pointer' : ''} isActive={pathname === url}>
+              <Link href={pathname === url ? '' : url}>
+                <Box center>
+                  {icon}
+                  <MainText gradient className='ml-2'>
+                    {name}
+                  </MainText>
+                </Box>
+              </Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+        <NavbarContent justify='end'>
+          {!!pendingTransactions.length && (
+            <Box center className='relative rounded-3xl border border-gray-500 bg-black/60 p-0.5'>
+              <MainText className='absolute'>{pendingTransactions.length}</MainText>
+              <Spinner />
+            </Box>
+          )}
+          <WalletModal />
+          <Box center className='rounded-md bg-red-900 px-3 py-1.5'>
+            <MainText className='text-xs'>Alpha v0.3.0</MainText>
+          </Box>
+        </NavbarContent>
+      </Navbar>
+    </>
   )
 }
---
+++ src/pages/analytics.tsx
@@ -0,0 +1,17 @@
+import { Box, Container, MainText } from '@/components/Layout'
+
+export default function NotFound() {
+  return (
+    <Container>
+      <Box col center>
+        <Box center className='relative mt-10 h-[40vw] w-[40vw] md:h-[30vw] md:w-[30vw]'>
+          <img src='https://placekitten.com/400/400' className='gradient-dark-element rounded-xl border' />
+        </Box>
+        <MainText gradient heading className='text-2xl'>
+          Coming soon
+        </MainText>
+        <MainText gradient>But here is a cat picture in the meantime</MainText>
+      </Box>
+    </Container>
+  )
+}
