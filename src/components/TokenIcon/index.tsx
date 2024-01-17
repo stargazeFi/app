ef17f08b7f14134525b626b11c034cb07376047d
0xK3K <0xk3k@protonmail.com>
2024-01-17 15:56:51 +0700

    Fix token icon bg

--- src/components/TokenIcon/index.tsx
+++ src/components/TokenIcon/index.tsx
@@ -1,7 +1,6 @@
-import { Box } from '@/components/Layout'
 import React, { useContext, useMemo } from 'react'
 import { TokenContext } from '@/contexts'
-import { Image } from '@nextui-org/react'
+import Image from 'next/image'
 
 interface TokenIconProps {
   address: string
@@ -16,11 +15,12 @@ export const TokenIcon = ({ address, className, size }: TokenIconProps) => {
   const icon = useMemo(() => (token ? `/assets/tokens/${token.symbol.toLowerCase()}.svg` : ''), [token])
 
   return (
-    <Box className='relative'>
-      <Image src={icon} width={size} height={size} className={`rounded-3xl border-2 border-gray-900 ${className}`} />
-      <Box className='absolute left-0 top-0 h-full w-full rounded-3xl bg-white'>
-        <div />
-      </Box>
-    </Box>
+    <Image
+      alt={token?.symbol || ''}
+      src={icon}
+      width={size}
+      height={size}
+      className={`rounded-3xl border-2 border-gray-900 bg-white ${className}`}
+    />
   )
 }
